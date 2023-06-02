8d8396c1905a8309d313a519a13983139c06677e
Akihiro Harai <jharai0815@gmail.com>
2023-06-02 10:28:37 +0900

    Support longer inline content #14

--- examples/client/go.mod
+++ examples/client/go.mod
@@ -1,8 +1,15 @@
 module github.com/tarosky/gutenberg-parallelefs/examples/client
 
-go 1.14
+go 1.20
 
 require (
-	github.com/sirupsen/logrus v1.5.0
-	github.com/urfave/cli/v2 v2.2.0
+	github.com/sirupsen/logrus v1.9.2
+	github.com/urfave/cli/v2 v2.25.5
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- reader.go
+++ reader.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
 	"io"
+	"net"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func connReader(conn io.Reader) <-chan []byte {
 	recvLine := make(chan []byte)
-	recv := bufio.NewScanner(conn)
+	recv := bufio.NewReader(conn)
 
 	// Abandon this goroutine on termination
 	// since conn.Read() blocks everything.
@@ -17,16 +20,38 @@ func connReader(conn io.Reader) <-chan []byte {
 		defer close(recvLine)
 
 		for {
-			if ok := recv.Scan(); !ok {
-				if recv.Err() != nil {
-					// This will be called when the connection is closed.
-					log.Debug(recv.Err())
+			temp, isPrefix, err := recv.ReadLine()
+			if err != nil {
+				if err != io.EOF && !errors.Is(err, net.ErrClosed) {
+					log.Error(err)
 				}
-
 				return
 			}
 
-			recvLine <- recv.Bytes()
+			buf := bytes.NewBuffer([]byte{})
+			buf.Write(temp)
+
+			if isPrefix {
+				for {
+					b, cont, err := recv.ReadLine()
+					if err != nil {
+						if err != io.EOF {
+							log.Fatal(err)
+						}
+						break
+					}
+
+					if _, err := buf.Write(b); err != nil {
+						log.Fatal(err)
+					}
+
+					if !cont {
+						break
+					}
+				}
+			}
+
+			recvLine <- buf.Bytes()
 		}
 	}()
 
